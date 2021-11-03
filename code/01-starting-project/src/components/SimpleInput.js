c76001b00d954d60609067aaf2ce552fdf2f5a15
Celalettin Turgut <celalettin.turgut@hotmail.com>
2021-11-03 11:13:05 +0100

    prettier config

--- code/01-starting-project/src/components/SimpleInput.js
+++ code/01-starting-project/src/components/SimpleInput.js
@@ -1,9 +1,19 @@
 const SimpleInput = (props) => {
+  const submitForm = (event) => {
+    event.preventDefault();
+    const nameElement = document.getElementById("name");
+    const emailElement = document.getElementById("email");
+
+    console.log(nameElement.value);
+    console.log(emailElement.value);
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitForm}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
