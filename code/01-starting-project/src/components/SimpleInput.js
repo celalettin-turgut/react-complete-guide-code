5e78efed17677e51ba65c921513eb7836bb9147e
Celalettin Turgut <celalettin.turgut@hotmail.com>
2021-11-03 14:17:53 +0100

    form validation

--- code/01-starting-project/src/components/SimpleInput.js
+++ code/01-starting-project/src/components/SimpleInput.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SimpleInput = (props) => {
+  const [nameInput, setNameInput] = useState('');
+  const [nameIsShort, setNameIsShort] = useState(true);
+  const [nameIsTouched, setNameIsTouched] = useState(false);
+
   const submitForm = (event) => {
     event.preventDefault();
-    const nameElement = document.getElementById('name');
-    const emailElement = document.getElementById('email');
+  };
 
-    console.log(nameElement.value);
-    console.log(emailElement.value);
+  const nameOnBlur = (e) => {
+    e.preventDefault();
+    setNameIsTouched(true);
+    if (nameInput.length > 2) {
+      setNameIsShort(false);
+    } else {
+      setNameIsShort(true);
+    }
   };
   return (
     <form onSubmit={submitForm}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+          onBlur={nameOnBlur}
+          type='text'
+          id='name'
+        />
+        {nameIsShort && nameIsTouched && (
+          <p style={{color: 'red'}}>Name cannot be empty</p>
+        )}
       </div>
       <div className='form-actions'>
         <button type='submit'>Submit</button>
